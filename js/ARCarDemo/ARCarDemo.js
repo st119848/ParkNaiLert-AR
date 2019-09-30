81e23eda9cb6d3cfc80b9be277cbd1cdc652ec48
Buzz Freeze <buzzfreeze@Buzzs-MacBook-Pro.local>
2019-09-30 12:32:49 +0700

    Add Image

--- js/ARCarDemo/ARCarDemo.js
+++ js/ARCarDemo/ARCarDemo.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-
 import { StyleSheet } from 'react-native';
-
 import ARData from './res/ARData.json';
 
 import {
@@ -20,108 +18,108 @@ import {
   ViroQuad,
   ViroText,
   ViroConstants,
-  ViroFlexView
+  ViroFlexView,
+  ViroARSceneNavigator
 } from 'react-viro';
 
 const createReactClass = require('create-react-class');
 
 const ARCarDemo = createReactClass({
 
-  allMarkers : ["logo", "logo2", "logo3"],
+  allMarkers: ["logo", "logo2", "logo3"],
 
   getInitialState() {
-    const baseState = 
-      {
-        texture: 'white',
-        playAnim: false,
-        animateCar: false,
-        isShow: false
-      }
+    const baseState =
+    {
+      texture: 'white',
+      playAnim: false,
+      animateCar: false,
+      isShow: false
+    }
     const varyState = {}
-    this.allMarkers.forEach( (marker) => {
+    this.allMarkers.forEach((marker) => {
       varyState['isShow' + marker] = false;
-     })
-    return {...baseState, ...varyState}
+    })
+    return { ...baseState, ...varyState }
   },
 
   render: function () {
-   
 
     return (
       <ViroARScene>
-        { this.allMarkers.map( (marker) => (
-          <ViroARImageMarker target={marker} onAnchorFound={ () => this._onAnchorFound(marker)} pauseUpdates={this.state.pauseUpdates}>
-          <Viro3DObject
-            scale={[0, 0, 0]}
-            source={require('./res/3D/jar.obj')}
-            resources={[require('./res/3D/jar.mtl'),]}
-            type="OBJ"
-            materials={this.state.texture}
-            onClick={this._toggleButtons}
-            position={[0.05, 0, 0.05]}
-            rotation={[0, 0, -90]}
-            visible={ this.state["isShow" + marker]}
-            animation={{ name: "scaleCar", run: this.state.animateCar, }} />
-          <ViroFlexView
-            rotation={[-90, -90, 0]}
-            height={0.03}
-            width={0.05}
-            style={styles.card}
-            visible={ this.state["isShow" + marker]}
-          >
+        {this.allMarkers.map((marker) => (
+          <ViroARImageMarker target={marker} onAnchorFound={() => this._onAnchorFound(marker)} pauseUpdates={this.state.pauseUpdates}>
+            <Viro3DObject
+              scale={[0, 0, 0]}
+              source={require('./res/3D/jar.obj')}
+              resources={[require('./res/3D/jar.mtl'),]}
+              type="OBJ"
+              materials={this.state.texture}
+              onClick={this._toggleButtons}
+              position={[0.05, 0, 0.05]}
+              rotation={[0, 0, -90]}
+              visible={this.state["isShow" + marker]}
+              animation={{ name: "scaleCar", run: this.state.animateCar, }} />
             <ViroFlexView
-              style={styles.cardWrapper}
+              rotation={[-90, -90, 0]}
+              height={0.03}
+              width={0.05}
+              style={styles.card}
+              visible={this.state["isShow" + marker]}
             >
-              <ViroText
-                textClipMode="None"
-                text={String(ARData.value[0].detail)}
-                scale={[.015, .015, .015]}
-                position={[-0.05, 0, 0.05]}
-                style={styles.textStyle}
-              />
+              <ViroFlexView
+                style={styles.cardWrapper}
+              >
+                <ViroText
+                  textClipMode="None"
+                  text={String(ARData.value[0].detail)}
+                  scale={[.015, .015, .015]}
+                  position={[-0.05, 0, 0.05]}
+                  style={styles.textStyle}
+                />
+              </ViroFlexView>
             </ViroFlexView>
-          </ViroFlexView>
-          <ViroSpotLight
-            innerAngle={5}
-            outerAngle={25}
-            direction={[0, -1, 0]}
-            position={[0, 5, 1]}
-            color="#ffffff"
-            castsShadow={true}
-            shadowMapSize={2048}
-            shadowNearZ={2}
-            shadowFarZ={7}
-            shadowOpacity={.7}
-          />
+            <ViroSpotLight
+              innerAngle={5}
+              outerAngle={25}
+              direction={[0, -1, 0]}
+              position={[0, 5, 1]}
+              color="#ffffff"
+              castsShadow={true}
+              shadowMapSize={2048}
+              shadowNearZ={2}
+              shadowFarZ={7}
+              shadowOpacity={.7}
+            />
 
-          <ViroQuad
-            rotation={[-90, 0, 0]}
-            position={[0, -0.001, 0]}
-            width={2.5} height={2.5}
-            arShadowReceiver={true}
-          />
+            <ViroQuad
+              rotation={[-90, 0, 0]}
+              position={[0, -0.001, 0]}
+              width={2.5} height={2.5}
+              arShadowReceiver={true}
+            />
 
-        </ViroARImageMarker>
+          </ViroARImageMarker>
         ))}
-        
+
       </ViroARScene>
     );
   },
   _onAnchorFound(marker) {
-    
+
     // Show only when isShow is all false 
     let allNotShow = true;
     console.log(marker);
-    this.allMarkers.forEach( (marker) => {
+    this.allMarkers.forEach((marker) => {
 
-       if(this.state['isShow' + marker] ){
-         console.log("all not show false ")
+      if (this.state['isShow' + marker]) {
+        console.log("all not show false ")
         allNotShow = false;
-       }
-     });
-    if(allNotShow){
-      let stateForSet = { animateCar: true};
-      stateForSet[ "isShow"+marker] = true;
+      }
+    });
+    if (allNotShow) {
+      let stateForSet = { animateCar: true };
+      stateForSet["isShow" + marker] = true;
       this.setState(stateForSet);
     }
   },
@@ -138,12 +136,12 @@ ViroARTrackingTargets.createTargets({
   logo: {
     source: require('./res/Test3.jpg'),
     orientation: "Left",
-    physicalWidth: 0.165 // real world width in meters
+    physicalWidth: ARData.physicalWidth // real world width in meters
   },
   logo2: {
     source: require('./res/logo2.png'),
     orientation: "Left",
-    physicalWidth: 0.165 // real world width in meters
+    physicalWidth: ARData.physicalWidth // real world width in meters
   },
   logo3: {
     source: require('./res/logo.png'),
