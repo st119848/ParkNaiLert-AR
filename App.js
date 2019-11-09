62489336c4d471bcb062c8bec51fa4ba63d2380d
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2019-11-10 01:17:49 +0700

    refactor file

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
-/**
- * Copyright (c) 2015-present, Viro Media, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import { Image, ImageBackground, StatusBar, StyleSheet, Text, TouchableOpacity, View, SegmentedControlIOS } from 'react-native';
-import SwipeUpDown from 'react-native-swipe-up-down';
-import PNLAR, { Testing } from './js/PNLAR/PNLAR';
-import ARData from './js/PNLAR/res/ARData.json';
-
+import React, { Component } from "react";
 import {
-  AppRegistry,
-} from 'react-native';
+	Image,
+	ImageBackground,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+	SegmentedControlIOS,
+} from "react-native";
+import SwipeUpDown from "react-native-swipe-up-down";
+import PNLAR, { Testing } from "./src/components/PNLAR/PNLAR";
+import ARData from "./assets/ARData.json";
+import { AppRegistry } from "react-native";
+import { ViroARSceneNavigator } from "react-viro";
 
-import {
-  ViroARSceneNavigator,
-} from 'react-viro';
-
-var createReactClass = require('create-react-class');
-
+var createReactClass = require("create-react-class");
 /*
  * TODO: Add your API key below!!
  */
@@ -38,75 +31,76 @@ var arScenes = {
 var showARScene = true;
 
 var ViroCodeSamplesSceneNavigator = createReactClass({
-  getInitialState(marker) {
-    const detailState =
-    {
-      textLangTitle: '',
-      textLangDetail: 'Tap to select the laguage',
-    }
-    return {
-      ...detailState
-    }
-  },
+	getInitialState(marker) {
+		const detailState = {
+			textLangTitle: "",
+			textLangDetail: "Tap to select the laguage",
+		};
+		return {
+			...detailState,
+		};
+	},
 
-  render: function () {
-    if (showARScene) {
-      return (
-        <View style={styles.viewStyle}>
-          <SwipeUpDown
-            itemMini={<Text style={styles.textStyle}>{this.state.textLangTitle}</Text>}// Pass props component when show full
-            itemFull={<Text style={styles.textStyle}>{this.state.textLangDetail}</Text>}// Pass props component when show full
-
-            onShowMini={() => console.log('mini')}
-            onShowFull={() => console.log('full')}
-            onMoveDown={() => console.log('down')}
-            onMoveUp={() => console.log('up')}
-            animation="easeInEaseOut"
-            swipeHeight={60} // Default 60
-            disablePressToShow={false} // Press item mini to show full
-            style={{
-              backgroundColor: 'white',
-              zIndex: 1,
-              borderTopLeftRadius: 10,
-              borderTopRightRadius: 10,
-              borderTopWidth: 5,
-              borderLeftWidth: 5,
-              borderRightWidth: 5,
-              borderColor: 'black',
-            }} // style for swipe
-          />
-          <ViroARSceneNavigator
-            initialScene={{ scene: PNLAR }}
-            apiKey={apiKey}
-            viroAppProps={{ onAnchored: this.onAnchored }}
-          />
-        </View>
-
-      );
-    }
-  },
-  onAnchored(marker) {
-    this.setState({
-      textLangTitle: String(ARData[marker - 1].value[0].title),
-      textLangDetail: String(ARData[marker - 1].value[0].detail),
-      tapJp: true
-    })
-  },
-})
+	render: function() {
+		if (showARScene) {
+			return (
+				<View style={styles.viewStyle}>
+					<SwipeUpDown
+						itemMini={
+							<Text style={styles.textStyle}>{this.state.textLangTitle}</Text>
+						} // Pass props component when show full
+						itemFull={
+							<Text style={styles.textStyle}>{this.state.textLangDetail}</Text>
+						} // Pass props component when show full
+						onShowMini={() => console.log("mini")}
+						onShowFull={() => console.log("full")}
+						onMoveDown={() => console.log("down")}
+						onMoveUp={() => console.log("up")}
+						animation='easeInEaseOut'
+						swipeHeight={60} // Default 60
+						disablePressToShow={false} // Press item mini to show full
+						style={{
+							backgroundColor: "white",
+							zIndex: 1,
+							borderTopLeftRadius: 10,
+							borderTopRightRadius: 10,
+							borderTopWidth: 5,
+							borderLeftWidth: 5,
+							borderRightWidth: 5,
+							borderColor: "black",
+						}} // style for swipe
+					/>
+					<ViroARSceneNavigator
+						initialScene={{ scene: PNLAR }}
+						apiKey={apiKey}
+						viroAppProps={{ onAnchored: this.onAnchored }}
+					/>
+				</View>
+			);
+		}
+	},
+	onAnchored(marker) {
+		this.setState({
+			textLangTitle: String(ARData[marker - 1].value[0].title),
+			textLangDetail: String(ARData[marker - 1].value[0].detail),
+			tapJp: true,
+		});
+	},
+});
 
 const styles = StyleSheet.create({
-  viewStyle: {
-    flex: 1,
-    backgroundColor: 'transparent'
-  },
-  textStyle: {
-    color: "#424242",
-    alignSelf: "flex-start",
-    fontSize: 20,
-    fontFamily: "Thonburi",
-    lineHeight: 30,
-    textAlign: "left"
-  }
+	viewStyle: {
+		flex: 1,
+		backgroundColor: "transparent",
+	},
+	textStyle: {
+		color: "#424242",
+		alignSelf: "flex-start",
+		fontSize: 20,
+		fontFamily: "Thonburi",
+		lineHeight: 30,
+		textAlign: "left",
+	},
 });
 
 // Uncomment the below line to use the ARDrivingCar Demo. Don't forget to set the apiKey variable in ARDrivingCar.js
---
+++ src/components/PNLAR/PNLAR.js
@@ -0,0 +1,202 @@
+"use strict";
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ARData from "../../../assets/ARData.json";
+
+import {
+	ViroARScene,
+	ViroMaterials,
+	ViroNode,
+	ViroAnimations,
+	ViroImage,
+	Viro3DObject,
+	ViroLightingEnvironment,
+	ViroARImageMarker,
+	ViroARTrackingTargets,
+	ViroSphere,
+	ViroSpotLight,
+	ViroQuad,
+	ViroText,
+	ViroConstants,
+	ViroFlexView,
+	ViroARSceneNavigator,
+} from "react-viro";
+
+const createReactClass = require("create-react-class");
+
+export const Testing = "Hi world";
+
+const PNLAR = createReactClass({
+	allMarkers: ["10"],
+
+	getInitialState(marker) {
+		const baseState = {
+			texture: "white",
+			textLangTitle: "",
+			textLangDetail: "Tap to select the laguage",
+			marker: "test",
+			playAnim: false,
+			animateObject: true,
+			isShow: false,
+			tapTh: false,
+			tapEn: false,
+			tapCh: false,
+			tapJp: false,
+		};
+		const varyState = {};
+		this.allMarkers.forEach(marker => {
+			varyState["isShow" + marker] = false;
+		});
+		return {
+			...baseState,
+			...varyState,
+		};
+	},
+
+	render: function() {
+		return (
+			<ViroARScene>
+				{this.allMarkers.map((marker, index) => (
+					<ViroARImageMarker
+						target={marker}
+						onAnchorFound={() =>
+							this.props.sceneNavigator.viroAppProps.onAnchored(marker)
+						}
+						key={index}
+						pauseUpdates={this.state.pauseUpdates}>
+						<Viro3DObject
+							scale={[0, 0, 0]}
+							source={require("../../../assets/3D/jar.obj")}
+							resources={[require("../../../assets/3D/jar.mtl")]}
+							type='OBJ'
+							materials={this.state.texture}
+							onClick={this._toggleButtons}
+							position={[0.05, 0, 0.05]}
+							rotation={[0, 0, -90]}
+							visible={this.state["isShow" + marker]}
+							animation={{ name: "scaleObject", run: this.state.animateObject }}
+						/>
+						<ViroSpotLight
+							innerAngle={5}
+							outerAngle={25}
+							direction={[0, -1, 0]}
+							position={[0, 5, 1]}
+							color='#ffffff'
+							castsShadow={true}
+							shadowMapSize={2048}
+							shadowNearZ={2}
+							shadowFarZ={7}
+							shadowOpacity={0.7}
+						/>
+						<ViroQuad
+							rotation={[-90, 0, 0]}
+							position={[0, -0.001, 0]}
+							width={2.5}
+							height={2.5}
+							arShadowReceiver={true}
+						/>
+					</ViroARImageMarker>
+				))}
+			</ViroARScene>
+		);
+	},
+	_onAnchorFound(marker) {
+		// Show only when isShow is all false
+		let allNotShow = true;
+		console.log(marker);
+		this.allMarkers.forEach(marker => {
+			if (this.state["isShow" + marker]) {
+				console.log("all not show false ");
+				allNotShow = false;
+			}
+		});
+		if (allNotShow) {
+			let stateForSet = { animateObject: true };
+			stateForSet["isShow" + marker] = true;
+			this.setState(stateForSet);
+		}
+	},
+	_toggleButtons() {
+		this.setState({
+			animName: this.state.animName == "scaleUp" ? "scaleDown" : "scaleUp",
+			playAnim: true,
+		});
+	},
+	_animateFinished() {
+		this.setState({
+			tapTh: false,
+			tapEn: false,
+			tapCh: false,
+			tapJp: false,
+		});
+	},
+});
+
+ViroMaterials.createMaterials({
+	white: {
+		shininess: 2.0,
+		lightingModel: "PBR",
+	},
+});
+
+ViroARTrackingTargets.createTargets({
+	"10": {
+		source: require("../../../assets/1.jpeg"),
+		orientation: "Left",
+		physicalWidth: ARData[0].physicalWidth, // real world width in meters
+	},
+});
+
+ViroAnimations.registerAnimations({
+	scaleUp: {
+		properties: { scaleX: 1, scaleY: 1, scaleZ: 1 },
+		duration: 500,
+		easing: "bounce",
+	},
+	scaleDown: {
+		properties: { scaleX: 0, scaleY: 0, scaleZ: 0 },
+		duration: 200,
+	},
+	scaleObject: {
+		properties: { scaleX: 0.002, scaleY: 0.002, scaleZ: 0.002 },
+		duration: 5000,
+		easing: "bounce",
+	},
+	scaleSphereUp: {
+		properties: { scaleX: 0.8, scaleY: 0.8, scaleZ: 0.8 },
+		duration: 50,
+		easing: "easeineaseout",
+	},
+	scaleSphereDown: {
+		properties: { scaleX: 1, scaleY: 1, scaleZ: 1 },
+		duration: 50,
+		easing: "easeineaseout",
+	},
+	tapAnimation: [["scaleSphereUp", "scaleSphereDown"]],
+});
+
+const styles = StyleSheet.create({
+	textStyle: {
+		flex: 0.5,
+		fontFamily: "Rosemary, Thonburi, Pingfang HK",
+		fontSize: 20,
+		color: "#ffffff",
+		lineHeight: 20,
+		textAlignVertical: "top",
+		textAlign: "left",
+		fontWeight: "bold",
+		includeFontPadding: false,
+	},
+	card: {
+		flexDirection: "column",
+	},
+	cardWrapper: {
+		flexDirection: "row",
+		alignItems: "flex-start",
+		padding: 0.001,
+		flex: 0.5,
+	},
+});
+
+export default PNLAR;
